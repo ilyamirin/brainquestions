db5f4037c3d04416836c85d1201e3643e11a5022
Ilya Mirin <ilya.mirin@gmail.com>
2014-04-16 17:21:57 +1100

    improve view

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,8 +7,10 @@
   	.click(function( event ) {    	
     	$( "a#timer-button" ).button( "option", "disabled", true );
     	$( "a#timer-button" ).hide();
-    	setTimeout(function() { $(".hidden").show(); $(".hidden").focus(); }, 60000);    	
+    	setTimeout(function() { $(".hidden").show(); $("#answer").focus(); }, 60000);    	
     	event.preventDefault();
   	});
+
+    $( "a#next-button" ).button();
 
   });
