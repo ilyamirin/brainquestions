6d10885b3a2b02aefb9b31cc4300637649a394c0
Ilya Mirin <ilya.mirin@gmail.com>
2014-04-17 10:47:02 +1100

    Add I know button

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,33 @@
 
-  $(function() {  	
-  	$(".hidden").hide();
+$(function() {  	
 
-    $( "a#timer-button" )
-	.button()
-  	.click(function( event ) {    	
-    	$( "a#timer-button" ).button( "option", "disabled", true );
-    	$( "a#timer-button" ).hide();
-    	setTimeout(function() { 
-    		$(".hidden").show(); $("#answer").focus(); 
-    		var answer_id = $( ".question" ).attr("id");
-    		$.getJSON('/do/not/show/answer/' + answer_id + '/to/user', function(res) {console.log(res);});
-    	}, 60000);    	
-    	event.preventDefault();
-  	});
+  var show_the_answer = function() { 
+    $(".hidden").show(); $("#answer").focus(); $( "#open-button" ).hide();
+    var answer_id = $( ".question" ).attr("id");
+    $.getJSON('/do/not/show/answer/' + answer_id + '/to/user', function(res) {console.log(res);});    
+  };
 
-    $( "a#next-button" ).button();
+  $(".hidden").hide();
 
+  $( "#open-button" ).button()
+  .click(function( event ) {
+    show_the_answer();
+    event.preventDefault();
   });
+
+  $( "a#timer-button" )
+  .button()
+  .click(function( event ) {    	
+  
+  	$( "a#timer-button" ).hide();
+    
+    $( "#open-button" ).parent().show();
+    
+    setTimeout(show_the_answer, 60000);    	
+
+    event.preventDefault();
+  });
+
+  $( "a#next-button" ).button();
+
+});
