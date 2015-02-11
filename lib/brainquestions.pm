18101b8f7c994156f7d809fb0719545d8ef476b8
ilyamirin <ilya.mirin@gmail.com>
2015-02-11 12:38:04 +1000

    fix bugs in main controller

--- lib/brainquestions.pm
+++ lib/brainquestions.pm
@@ -1,6 +1,7 @@
 package brainquestions;
 use Dancer ':syntax';
 
+use boolean;
 use MongoDB;
 use MongoDB::OID;
 
@@ -11,8 +12,14 @@ my $database  = $client->get_database( 'braindo' );
 my $questions = $database->get_collection( 'questions' );
 
 get '/' => sub {
-	my $questions_count = $questions->count;	
-	my $question = $questions->find->limit(-1)->skip(int rand $questions_count )->next;
+	my $questions_count = $questions->count({isValid => true});	
+    info 'count= ' . $questions_count;
+	my $question = $questions->
+        find({isValid => true})->
+        limit(-1)->
+        skip(int rand $questions_count )->
+        next;
+    $question->{authorName} = 'Некто' unless $question->{authorName};
 	template 'index', { question => $question };
 };
 
